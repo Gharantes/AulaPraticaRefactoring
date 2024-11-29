143775dfcd7383a1c5cf5790d3bf2408c8572372
Gharantes <work.gharantes@gmail.com>
2024-11-29 19:43:17 -0300

    Commit 8

--- Customer.java
+++ Customer.java
@@ -51,4 +51,20 @@ public class Customer {
         }
         return result;
     }
+
+    public String htmlStatement() {
+        Enumeration rentals = _rentals.elements();
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getMovie().getTitle()+ ": " +
+                    each.getCharge() + "<BR>\n";
+        }
+
+        result +=  "<P>You owe <EM>" + getTotalCharge() + "</EM><P>\n";
+        result += "On this rental you earned <EM>" +
+                getTotalFrequentRenterPoints() +
+                "</EM> frequent renter points<P>";
+        return result;
+    }
 }
